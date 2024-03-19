d824082cf416d92b17a58c5d8d70430560d92a06
shixuan.song <bettercallssx@gmail.com>
2024-03-19 23:54:31 +0800

    log

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,16 @@ const mockArticleData: ArticleData = {
 };
 
 export default function Home() {
+    const memoryUsageInBytes = process.memoryUsage();
+    const memoryUsageInMB = {
+        rss: (memoryUsageInBytes.rss / 1024 / 1024).toFixed(2),
+        heapTotal: (memoryUsageInBytes.heapTotal / 1024 / 1024).toFixed(2),
+        heapUsed: (memoryUsageInBytes.heapUsed / 1024 / 1024).toFixed(2),
+        external: (memoryUsageInBytes.external / 1024 / 1024).toFixed(2),
+        arrayBuffers: (memoryUsageInBytes.arrayBuffers / 1024 / 1024).toFixed(2)
+    };
+    console.log("Memory usage in MB: ", memoryUsageInMB);
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-5">
             <div className="flex flex-col items-center justify-start min-h-screen py-2">
