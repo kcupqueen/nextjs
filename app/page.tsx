7300243011ad6c6605d5cc33a3d4ee48fd73308e
shixuan.song <bettercallssx@gmail.com>
2024-03-20 20:08:55 +0800

    ui refresh article

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
 import Image from "next/image";
 import Article from "@/app/ui/default/article";
-import {ArticleData} from "@/app/ui/default/article";
-const mockArticleData: ArticleData = {
-    _id: "123456",
-    level: 700,
-    title: "Sample Article",
-    content: "This is a sample article content.",
-    question: "how did you feel about this article?",
-    answers: ["Answer 1", "Answer 2", "Answer 3"],
-    correctAnsIdx: 0, // suppose the correct answer is the first one
-};
+import GenerateMockDate from "@/app/lib/default/mock_article";
 
 export default function Home() {
-    const memoryUsageInBytes = process.memoryUsage();
-    const memoryUsageInMB = {
-        rss: (memoryUsageInBytes.rss / 1024 / 1024).toFixed(2),
-        heapTotal: (memoryUsageInBytes.heapTotal / 1024 / 1024).toFixed(2),
-        heapUsed: (memoryUsageInBytes.heapUsed / 1024 / 1024).toFixed(2),
-        external: (memoryUsageInBytes.external / 1024 / 1024).toFixed(2),
-        arrayBuffers: (memoryUsageInBytes.arrayBuffers / 1024 / 1024).toFixed(2)
-    };
-    console.log("Memory usage in MB: ", memoryUsageInMB);
-
+    const mockArticleData = GenerateMockDate()
+    // generate random level from 500~1000
+    const level = Math.floor(Math.random() * 500) + 500
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-5">
             <div className="flex flex-col items-center justify-start min-h-screen py-2">
                 <h1 className="text-3xl font-bold mb-4">Latest Article</h1>
-                <Article data={mockArticleData}/>
+                <Article list={mockArticleData} filterLevel={level}/>
             </div>
         </main>
     );
--- app/ui/default/article.tsx
+++ app/ui/default/article.tsx
@@ -1,15 +1,6 @@
 'use client';
-import {useState, ChangeEvent} from 'react';
-import Link from 'next/link';
-import {
-    UserGroupIcon,
-    HomeIcon,
-    ArrowRightIcon,
-} from '@heroicons/react/24/outline';
-import {  usePathname, useSearchParams } from 'next/navigation';
+import {ChangeEvent, useState} from 'react';
 
-
-import clsx from 'clsx'; // css conditional classes
 
 export type ArticleData = {
     _id: string;
@@ -21,42 +12,73 @@ export type ArticleData = {
     correctAnsIdx: number,
 }
 
-function Article({data}: { data: ArticleData }) {
-    const {title, content, question, answers, correctAnsIdx, level} = data;
+const FilterDataCallback = (list: ArticleData[], filterLevel: number, changObj?: (obj: ArticleData) => void) => {
+    const data = list.filter((item) => {
+        // range -50 +50
+        return item.level >= filterLevel - 50 && item.level <= filterLevel + 50;
+    })[0];
+    if (data && changObj) {
+        changObj(data);
+        console.log(`change to ${data.level}`)
+    }
+    return data
+}
+let globalCacheList: ArticleData[] = []
+function Article({list, filterLevel}: { list: ArticleData[] , filterLevel: number}) {
+    globalCacheList = list;
+
+    const [levelNow, setLevel] = useState(filterLevel);
+
+    const data = FilterDataCallback(list, levelNow);
+    const [articleObj, setArticleObj] = useState<ArticleData>(data);
+    const cloned = {...articleObj};
+    const {title, content, question, answers, correctAnsIdx, level} = cloned;
+
+
     return (
         <>
             <div
                 className="bg-white rounded-lg shadow-md p-4 md:p-6 m-4 md:m-8 flex flex-col items-center justify-center w-full">
                 <h2 className="text-xl font-semibold">{title}</h2>
                 <p className="text-gray-700 mt-2">{content}</p>
+                <p className="text-gray-700 mt-2">Level: {levelNow}</p>
             </div>
             <div>
-                <RadioGroup answers={answers} question={question} correctIdx={correctAnsIdx} level={level}/>
+                <RadioGroup answers={answers} question={question}
+                            correctIdx={correctAnsIdx}  levelNow={levelNow} setLevel={setLevel}
+                            setArticleObj={setArticleObj}
+                />
             </div>
         </>
     );
 }
 
 
-const RadioGroup = ({question, answers, correctIdx, level}: {
+const RadioGroup = ({question, answers, correctIdx, levelNow, setLevel, setArticleObj}: {
     question: string,
     answers: string[],
     correctIdx: number,
-    level: number
+    levelNow: number,
+    setLevel: (level: number) => void,
+    setArticleObj?: (obj: ArticleData) => void,
 }) => {
-
     const [selectedOption, setSelectedOption] = useState('');
 
-    const [levelNow, setLevel] = useState(level);
-
     let correct = false;
-    let nextLevel = level;
+    let nextLevel = levelNow;
     const handleOptionChange = (event: ChangeEvent<HTMLInputElement>) => {
         setSelectedOption(event.target.value);
         correct = parseInt(event.target.value) === correctIdx;
-        nextLevel = correct ? level + 100 : level - 100;
+        nextLevel = correct ? levelNow + 100 : levelNow - 100;
         setLevel(nextLevel);
     };
+
+    const handleOnclickOfButton = () => {
+        if (setArticleObj) {
+            FilterDataCallback(globalCacheList, nextLevel, setArticleObj);
+        }
+    }
+
 
     const radioButtons = answers.map((ans, index) => {
         return (
@@ -73,6 +95,7 @@ const RadioGroup = ({question, answers, correctIdx, level}: {
         );
     });
 
+
     return (
         <div className="flex flex-col space-y-2">
             <label className="font-bold">{question}</label>
@@ -82,10 +105,7 @@ const RadioGroup = ({question, answers, correctIdx, level}: {
             <p className="text-gray-500">Selected: {selectedOption}</p>
             <div>
                 <button
-                    onClick={() => {
-                        // levelNow
-                        console.log('levelNow', levelNow);
-                    }}
+                    onClick={handleOnclickOfButton}
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
                     Button
                 </button>
